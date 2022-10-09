05e780f61168c3dd484cdf0c2a58a0934c870681
chetanbhale <78893629+chetanbhale@users.noreply.github.com>
2022-10-09 13:10:08 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent implements OnInit {
+  name = 'Angular';
+  legalGroups!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.legalGroups = this.formBuilder.group({
+      legalEntity: this.formBuilder.array([this.createLegalFormGroup()]),
+    });
+  }
+  private createLegalFormGroup(): FormGroup {
+    return new FormGroup({
+      name: new FormControl(''),
+      email: new FormControl(''),
+      contact: new FormControl(''),
+    });
+  }
+
+  public addLegalFormGroup() {
+    const entity = this.legalGroups.get('legalEntity') as FormArray;
+    entity.push(this.createLegalFormGroup());
+  }
 }
